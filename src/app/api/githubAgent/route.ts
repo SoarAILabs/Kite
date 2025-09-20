36475b99b84a23136fdd2de60aa3ed6e9975ce26
Amaan Bilwar <bilwarad@mail.uc.edu>
2025-09-20 02:41:59 -0400

    delete

--- src/app/api/githubAgent/route.ts
+++
@@ -1,9 +0,0 @@
-class GithubAgent {
-  async processRequest(request: string) {
-    const response = await fetch("/api/github", {
-      method: "POST",
-      body: request,
-    });
-    return response.json();
-  }
-}
